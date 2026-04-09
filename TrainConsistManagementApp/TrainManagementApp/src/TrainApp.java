968c26e4b6beb8044e0f032e4cb49f6d1282545d
jk5453 <jk5453@srmist.edu.in>
2026-04-09 16:26:26 +0530

    UC1

---
+++ TrainConsistManagementApp/TrainManagementApp/src/TrainApp.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class TrainApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== Train Consist Management App ===\n");
+
+        List<String> trainConsist = new ArrayList<>();
+
+        System.out.println("Train initialized successfully...");
+        System.out.println("Initial Bogie Count: " + trainConsist.size());
+        System.out.println("Current Train Consist: " + trainConsist);
+        System.out.println("System ready for operations...");
+    }
+}
